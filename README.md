6414301f8a8a2124c16c54c450df22bd11931c30
Walmyr <wlsf82@gmail.com>
2022-07-14 21:50:17 +0200

    Create cypress project and app under test

---
+++ README.md
@@ -0,0 +1,3 @@
+# remove-imgs
+
+Sample project to demonstrate the usage of [`prev()`](https://docs.cypress.io/api/commands/prev) and [`next()`](https://docs.cypress.io/api/commands/next) Cypress commands.
---
+++ cypress/e2e/removeImages.cy.js
@@ -0,0 +1,17 @@
+describe('Remove images', () => {
+  it('successfully', () => {
+    cy.visit('./src/index.html')
+
+    cy.get('img[src="./img1.png"]')
+      .next()
+      .click()
+      .prev()
+      .should('have.attr', 'src', './empty.png')
+
+    cy.get('img[src="./img2.png"]')
+      .next()
+      .click()
+      .prev()
+      .should('have.attr', 'src', './empty.png')
+  })
+})
